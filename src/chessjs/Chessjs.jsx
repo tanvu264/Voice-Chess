5824f6b4a020bb0d245874450994dfecbe4f1956
Tan Vu <67347281+tanvu264@users.noreply.github.com>
2024-01-03 22:52:14 -0800

    UI/UX update

--- src/chessjs/Chessjs.jsx
+++ src/chessjs/Chessjs.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { Chess } from "chess.js";
 import { Chessboard } from "react-chessboard";
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
+import "./chessjs.css"
 
 export default function Game() {
   const [game, setGame] = useState(new Chess());
   const startListening = () => SpeechRecognition.startListening({continuous:true, language: 'en-US'})
+  const stopListening = () => SpeechRecognition.stopListening()
   const {transcript, resetTranscript, browserSupportsSpeechRecognition} = useSpeechRecognition()
   
   if (!browserSupportsSpeechRecognition) {
@@ -37,48 +39,52 @@ export default function Game() {
   }
 
   function srcSquare() {
-    return document.getElementById("moveStart").value 
+    let myJSON = JSON.stringify({transcript}) // round-a-bout way to convert transcript obj to string
+    let myObj = JSON.parse(myJSON) // first convert to json string then to javascript object
+    let myArr = myObj.transcript.split(" ")
+    return myArr[0].toLowerCase()
   }
 
   function trgSquare() {
-    return document.getElementById("moveEnd").value 
-  }
-
-  function setStartSquare() {
-    let myJSON = JSON.stringify({transcript}) // round-a-bout way to convert transcript obj to string
-    let myObj = JSON.parse(myJSON) // first convert to json string then to javascript object
-    return document.getElementById("moveStart").value = myObj.transcript.toLowerCase()
-  }
-
-  function setEndSquare() {
     let myJSON = JSON.stringify({transcript})
     let myObj = JSON.parse(myJSON)
-    return document.getElementById("moveEnd").value = myObj.transcript.toLowerCase()
+    let myArr = myObj.transcript.split(" ")
+    return myArr[1].toLowerCase()
   }
 
   return (
   <div>
-    {transcript}
-    <br/>
-    <label htmlFor="choosePiece">PROMOTE</label>
-    <select onChange="promote()" id="choosePiece">
-      <option value="q">Queen</option>
-      <option value="r">Rook</option>
-      <option value="b">Bishop</option>
-      <option value="n">Knight</option>
-    </select>
+    <div class="fboxcontainer">
+      <div class="leftcol">
+        <h3>Voice Chess</h3>
+        <p>
+          To move a piece, say the coordinates of the starting square 
+          followed by the target square.
+          <br/>
+          <br/>
+          For example, you might make your first move by saying: "E2 E4"
+        </p>
+        <div class="transcriptbox">
+          {transcript}
+        </div>
+        <button onClick={startListening}>Start Listening</button>
+        <button onClick={stopListening}>Stop Listening</button> 
+        <button onClick={resetTranscript}>Reset</button>
+        <button onClick={onDrop}>Submit Move</button>
+      </div>
 
-    <button onClick={startListening}>Start Listening</button>
-    <button onClick={SpeechRecognition.stopListening}>Stop Listening</button> 
-    <button onClick={resetTranscript}>Reset</button>
-
-    <input type="text" id="moveStart" placeholder="Enter a square e.g. d4" />
-    <input type="text" id="moveEnd" placeholder="Enter a square e.g. d4" />
-    <button onClick={setStartSquare}>Confirm Start Square</button>
-    <button onClick={setEndSquare}>Confirm End Square</button>
-    <button onClick={onDrop}>Submit Move</button>
-
-    <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+      <div>
+        <label htmlFor="choosePiece">Promote </label>
+        <select onChange="promote()" id="choosePiece">
+          <option value="q">Queen</option>
+          <option value="r">Rook</option>
+          <option value="b">Bishop</option>
+          <option value="n">Knight</option>
+        </select>
+        <Chessboard position={game.fen()} onPieceDrop={onDrop} />
+      </div>
+    </div>
+    
   </div> 
   );
 }
